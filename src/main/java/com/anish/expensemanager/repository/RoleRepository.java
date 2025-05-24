6537917e17534c1c8c06388a885b7339d2ab4905
Anish Chauhan <chauhananish92@gmail.com>
2025-05-24 22:40:03 +0530

    Updated CI

---
+++ src/main/java/com/anish/expensemanager/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.anish.expensemanager.repository;
+
+import com.anish.expensemanager.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
